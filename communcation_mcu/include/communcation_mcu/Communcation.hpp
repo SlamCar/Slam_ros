293b729027cf7cf5d1d27d884263d64a8a605a62
wangjy <wangjy@cotek-robotics.com>
2018-07-17 15:26:09 +0800

    new start

---
+++ communcation_mcu/include/communcation_mcu/Communcation.hpp
@@ -0,0 +1,11 @@
+/** 
+ * Copyright (c) 2018 old_wang Inc. All rights reserved. 
+ */
+#pragma once
+
+
+
+class SwarmClient
+{
+
+};
---
+++ communcation_mcu/src/mcu_node.cpp
@@ -0,0 +1,24 @@
+#include "Communcation.hpp"
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mcu_node");
+
+    // Set ros log level:
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info))
+    {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
+    // if (!swarm::SwarmClient::getInstance().start())
+    // {
+    //     return -1;
+    // }
+
+    ROS_INFO_STREAM("MCU communcation started...");
+
+    ros::spin();
+
+    return 0;
+}
