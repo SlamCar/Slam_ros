cf6f62be98e25d6ccede07a1a298f5626e509e33
wangjy <wangjy@cotek-robotics.com>
2018-09-10 16:02:42 +0800

    add test

--- navigation/test/test_point.cpp
+++ navigation/test/test_point.cpp
@@ -14,8 +14,38 @@ int main(int argc, char **argv)
 TEST(Point, basics)
 {
     {
+        Point p1;
+        EXPECT_DOUBLE_EQ(0.0, p1.x());
+        EXPECT_DOUBLE_EQ(0.0, p1.y());
+    
+        Point p2(1.0, 2.0);
+        EXPECT_DOUBLE_EQ(1.0, p2.x());
+        EXPECT_DOUBLE_EQ(2.0, p2.y());
+    
+        Point p3(p2);
+        EXPECT_DOUBLE_EQ(1.0, p2.x());
+        EXPECT_DOUBLE_EQ(2.0, p2.y());
+
+        Point p4 = p2;
+        EXPECT_DOUBLE_EQ(1.0, p2.x());
+        EXPECT_DOUBLE_EQ(2.0, p2.y());
+    }
+
+    {
         Point p;
-        EXPECT_DOUBLE_EQ(0.0, p.x());
-        EXPECT_DOUBLE_EQ(0.0, p.y());
+        p.setX(1.1);
+        p.setY(2.2);
+        EXPECT_DOUBLE_EQ(1.1, p.x());
+        EXPECT_DOUBLE_EQ(2.2, p.y());
+
+        p.setXY(2.2, 1.1);
+        EXPECT_DOUBLE_EQ(2.2, p.x());
+        EXPECT_DOUBLE_EQ(1.1, p.y());
+    }   
+
+    {
+        Point p1(0.0, 0.0);
+        Point p2(1.0, 1.0);
+        EXPECT_DOUBLE_EQ(1.413999999999, p1.getDistance(p2));
     }
 }
